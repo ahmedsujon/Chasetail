8e1ce586320916bef6660d464a0e036bfdfd4b97
ahmedsujon <sujonahmed424@gmail.com>
2024-01-25 10:45:14 +0600

    lost & found dog

---
+++ app/Livewire/App/FoundDog/FoundDogComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\FoundDog;
+
+use Livewire\Component;
+
+class FoundDogComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.found-dog.found-dog-component');
+    }
+}
---
+++ database/seeders/LostDogSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\LostDog;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class LostDogSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        foreach (range(1, 100) as $index) {
+            LostDog::create([
+                'user_id' => $faker->randomDigit(1,10),
+                'payment_status' => $faker->randomElement(['paid', 'unpaid']),
+                'name' => $faker->firstName,
+                'gender' => $faker->randomElement(['male', 'female']),
+                'color' => $faker->colorName,
+                'description' => $faker->text,
+                'longitude' => $faker->longitude,
+                'latitude' => $faker->latitude,
+                'missing_status' => $faker->randomElement(['found', 'searching', 'not_found']),
+                'status' => $faker->randomDigit(0,1),
+            ]);
+        }
+    }
+}
