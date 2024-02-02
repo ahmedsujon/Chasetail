1e9887ad4667ed4e79fbbc9e612170251ed0f107
ahmedsujon <sujonahmed424@gmail.com>
2024-02-02 17:17:35 +0600

    home components added

--- resources/views/livewire/app/home-component.blade.php
+++ resources/views/livewire/app/home-component.blade.php
@@ -1,17 +1,10 @@
 <div>
     <section id="banner">
         <div class="banner">
-            <div class="container-fluid px-0">
-                <div class="row g-0">
-                    <div class="col-md-12 col-lg-12 col-12">
-                        <img class="img-fluid" src="{{ asset('assets/app/images/banner01.jpg') }}" alt="Banner">
-                    </div>
-                </div>
-            </div>
             <div class="banner-text">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-7 col-lg-8 col-7">
+                        <div class="col-md-9 col-lg-8 col-9">
                             <h3>Do You Want To find
                                 your lost dog? </h3>
                             <a href="#">Get Started</a>
@@ -28,205 +21,13 @@
             <div class="col-md-6 col-lg-6 col-sm-12">
                 <div class="content-box">
                     <h2>List of Found Dogs</h2>
-                    <div class="box-info">
-                        <div class="row">
-                            <div class="col-md-5 col-lg-5 col-5">
-                                <div class="box-info-img">
-                                    <img class="img-fluid" src="{{ asset('assets/app/images/found-dog-1.jpg') }}"
-                                        alt="found dog" />
-                                </div>
-                            </div>
-                            <div class="col-md-7 col-lg-7 col-7">
-                                <div class="box-info-text">
-                                    <h3>Pomeranian Puppy</h3>
-                                    <p>Found Address: Dallas - Texas</p>
-                                    <p>Color: White</p>
-                                    <p>Male</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="box-info">
-                        <div class="row">
-                            <div class="col-md-5 col-lg-5 col-5">
-                                <div class="box-info-img">
-                                    <img class="img-fluid" src="{{ asset('assets/app/images/found-dog-2.jpg') }}"
-                                        alt="found dog" />
-                                </div>
-                            </div>
-                            <div class="col-md-7 col-lg-7 col-7">
-                                <div class="box-info-text">
-                                    <h3>Toy Poodle</h3>
-                                    <p>Found Address: Dallas - Texas</p>
-                                    <p>Color: Brown</p>
-                                    <p>Male</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="box-info">
-                        <div class="row">
-                            <div class="col-md-5 col-lg-5 col-5">
-                                <div class="box-info-img">
-                                    <img class="img-fluid" src="{{ asset('assets/app/images/found-dog-3.jpg') }}"
-                                        alt="found dog" />
-                                </div>
-                            </div>
-                            <div class="col-md-7 col-lg-7 col-7">
-                                <div class="box-info-text">
-                                    <h3>Affenpinscher</h3>
-                                    <p>Found Address: Dallas - Texas</p>
-                                    <p>Color: Black</p>
-                                    <p>Male</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="box-info">
-                        <div class="row">
-                            <div class="col-md-5 col-lg-5 col-5">
-                                <div class="box-info-img">
-                                    <img class="img-fluid" src="{{ asset('assets/app/images/found-dog-4.jpg') }}"
-                                        alt="found dog" />
-                                </div>
-                            </div>
-                            <div class="col-md-7 col-lg-7 col-7">
-                                <div class="box-info-text">
-                                    <h3>Japanese Chin</h3>
-                                    <p>Found Address: Dallas - Texas</p>
-                                    <p>Color: White & Brown Mix</p>
-                                    <p>Male</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="box-info">
-                        <div class="row">
-                            <div class="col-md-5 col-lg-5 col-5">
-                                <div class="box-info-img">
-                                    <img class="img-fluid" src="{{ asset('assets/app/images/found-dog-5.jpg') }}"
-                                        alt="found dog" />
-                                </div>
-                            </div>
-                            <div class="col-md-7 col-lg-7 col-7">
-                                <div class="box-info-text">
-                                    <h3>Biewer Terrier</h3>
-                                    <p>Found Address: Dallas - Texas</p>
-                                    <p>Color: Black</p>
-                                    <p>Male</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="view-all">
-                        <div class="row">
-                            <div class="col-md-12 text-center">
-                                <a href="#" class="btn-view">View All</a>
-                            </div>
-                        </div>
-                    </div>
+                    <livewire:app.components.lost-dog-component lazy />
                 </div>
             </div>
             <div class="col-md-6 col-lg-6 col-sm-12">
                 <div class="content-box">
                     <h2>List of Lost Dogs</h2>
-                    <div class="box-info">
-                        <div class="row">
-                            <div class="col-md-5 col-lg-5 col-5">
-                                <div class="box-info-img">
-                                    <img class="img-fluid" src="{{ asset('assets/app/images/lost-dog-1.jpg') }}"
-                                        alt="lost dog" />
-                                </div>
-                            </div>
-                            <div class="col-md-7 col-lg-7 col-7">
-                                <div class="box-info-text">
-                                    <h3>Papillon</h3>
-                                    <p>Found Address: Dallas - Texas</p>
-                                    <p>Color: White & Brown Mix</p>
-                                    <p>Male</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="box-info">
-                        <div class="row">
-                            <div class="col-md-5 col-lg-5 col-5">
-                                <div class="box-info-img">
-                                    <img class="img-fluid" src="{{ asset('assets/app/images/lost-dog-2.jpg') }}"
-                                        alt="lost dog" />
-                                </div>
-                            </div>
-                            <div class="col-md-7 col-lg-7 col-7">
-                                <div class="box-info-text">
-                                    <h3>Chihuahua Puppy</h3>
-                                    <p>Found Address: Dallas - Texas</p>
-                                    <p>Color: White</p>
-                                    <p>Male</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="box-info">
-                        <div class="row">
-                            <div class="col-md-5 col-lg-5 col-5">
-                                <div class="box-info-img">
-                                    <img class="img-fluid" src="{{ asset('assets/app/images/lost-dog-3.jpg') }}"
-                                        alt="lost dog" />
-                                </div>
-                            </div>
-                            <div class="col-md-7 col-lg-7 col-7">
-                                <div class="box-info-text">
-                                    <h3>Yorkshire Terrier</h3>
-                                    <p>Found Address: Dallas - Texas</p>
-                                    <p>Color: Brown</p>
-                                    <p>Male</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="box-info">
-                        <div class="row">
-                            <div class="col-md-5 col-lg-5 col-5">
-                                <div class="box-info-img">
-                                    <img class="img-fluid" src="{{ asset('assets/app/images/lost-dog-4.jpg') }}"
-                                        alt="lost dog" />
-                                </div>
-                            </div>
-                            <div class="col-md-7 col-lg-7 col-7">
-                                <div class="box-info-text">
-                                    <h3>Chinese Crested</h3>
-                                    <p>Found Address: Dallas - Texas</p>
-                                    <p>Color: White</p>
-                                    <p>Male</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="box-info">
-                        <div class="row">
-                            <div class="col-md-5 col-lg-5 col-5">
-                                <div class="box-info-img">
-                                    <img class="img-fluid" src="{{ asset('assets/app/images/lost-dog-5.jpg') }}"
-                                        alt="lost dog" />
-                                </div>
-                            </div>
-                            <div class="col-md-7 col-lg-7 col-7">
-                                <div class="box-info-text">
-                                    <h3>Shih Tzu</h3>
-                                    <p>Found Address: Dallas - Texas</p>
-                                    <p>Color: White</p>
-                                    <p>Male</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="view-all">
-                        <div class="row">
-                            <div class="col-md-12 text-center">
-                                <a href="#" class="btn-view">View All</a>
-                            </div>
-                        </div>
-                    </div>
+                    <livewire:app.components.found-dog-component lazy />
                 </div>
             </div>
         </div>
