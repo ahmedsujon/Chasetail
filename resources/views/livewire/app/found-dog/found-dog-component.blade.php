0fdbdb958dc7b4c4a4e8b85b028b87d5e0051813
ahmedsujon <sujonahmed424@gmail.com>
2024-02-05 10:48:55 +0600

    list view

--- resources/views/livewire/app/found-dog/found-dog-component.blade.php
+++ resources/views/livewire/app/found-dog/found-dog-component.blade.php
@@ -1,3 +1,33 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12 col-sm-12">
+                <div class="content-box">
+                    <h2>List of Found Dogs</h2>
+                    @foreach ($found_dogs as $found_dog)
+                        <div class="box-info">
+                            <div class="row">
+                                <div class="col-md-5 col-lg-5 col-5">
+                                    <div class="box-info-img">
+                                        <img class="img-fluid" src="{{ asset($found_dog->photos) }}" alt="found dog" />
+                                    </div>
+                                </div>
+                                <div class="col-md-7 col-lg-7 col-7">
+                                    <div class="box-info-text">
+                                        <h3>{{ $found_dog->name }}</h3>
+                                        <p>Found Address: {{ $found_dog->address }}</p>
+                                        <p>Color: {{ $found_dog->color }}</p>
+                                        <p>Gender: {{ $found_dog->gender }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="card-footer bg-transparent">
+                    {{ $found_dogs->links('livewire.pagination-links') }}
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/livewire/app/lost-dog/lost-dog-component.blade.php
+++ resources/views/livewire/app/lost-dog/lost-dog-component.blade.php
@@ -1,3 +1,33 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12 col-sm-12">
+                <div class="content-box">
+                    <h2>List of Lost Dogs</h2>
+                    @foreach ($lost_dogs as $lost_dog)
+                        <div class="box-info">
+                            <div class="row">
+                                <div class="col-md-5 col-lg-5 col-5">
+                                    <div class="box-info-img">
+                                        <img class="img-fluid" src="{{ asset($lost_dog->photos) }}" alt="found dog" />
+                                    </div>
+                                </div>
+                                <div class="col-md-7 col-lg-7 col-7">
+                                    <div class="box-info-text">
+                                        <h3>{{ $lost_dog->name }}</h3> 
+                                        <p>Found Address: {{ $lost_dog->address }}</p>
+                                        <p>Color: {{ $lost_dog->color }}</p>
+                                        <p>Gender: {{ $lost_dog->gender }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="card-footer bg-transparent">
+                    {{ $lost_dogs->links('livewire.pagination-links') }}
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
