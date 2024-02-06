e01faee1d9fb654f0a3f1f871d8ecdc8e35c4ccf
ahmedsujon <sujonahmed424@gmail.com>
2024-02-06 22:28:34 +0600

    placeholder fixed

--- app/Livewire/App/Components/FoundDogComponent.php
+++ app/Livewire/App/Components/FoundDogComponent.php
@@ -7,8 +7,14 @@ use Livewire\Component;
 
 class FoundDogComponent extends Component
 {
+
+    public function placeholder()
+    {
+        return view('livewire.app.placeholders.found-dog');
+    }
     public function render()
     {
+        sleep(5);
         $found_dogs = FoundDog::orderBy("created_at","desc")->take(5)->get();
         return view('livewire.app.components.found-dog-component', ['found_dogs'=>$found_dogs])->layout('livewire.app.layouts.base');
     }
---
+++ resources/views/livewire/app/placeholders/found-dog.blade.php
@@ -0,0 +1,247 @@
+<div>
+    <style>
+        .card {
+            border: none;
+            width: 100%;
+            border-radius: 5px;
+            overflow: hidden;
+            background: #fff8ef;
+        }
+
+        .inner-card {
+            height: 20px;
+            border-radius: 10px;
+            background-color: #eee;
+        }
+
+        .card-1 {
+            height: 158px;
+            background-color: #eee;
+            border-radius: 20px;
+            border: 2px solid #eee;
+        }
+
+        .card-2 {
+            height: 158px;
+        }
+
+        .h-screen {
+            height: 100vh;
+        }
+
+        .animate-pulse {
+            animation: pulse 2s cubic-bezier(.4, 0, .6, 1) infinite;
+        }
+
+        @keyframes pulse {
+            50% {
+                opacity: .9;
+            }
+        }
+    </style>
+    <div class="found_dog_skeleton d-flex justify-content-center align-items-center">
+        <div class="card">
+            <div class="row">
+                <div class="col-5">
+                    <div class="card-1 animate-pulse">
+                    </div>
+                </div>
+                <div class="col-7">
+                    <div class="card-2 p-3">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="inner-card animate-pulse">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-10">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-6">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-4">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="found_dog_skeleton d-flex justify-content-center align-items-center">
+        <div class="card">
+            <div class="row">
+                <div class="col-5">
+                    <div class="card-1 animate-pulse">
+                    </div>
+                </div>
+                <div class="col-7">
+                    <div class="card-2 p-3">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="inner-card animate-pulse">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-10">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-6">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-4">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="found_dog_skeleton d-flex justify-content-center align-items-center">
+        <div class="card">
+            <div class="row">
+                <div class="col-5">
+                    <div class="card-1 animate-pulse">
+                    </div>
+                </div>
+                <div class="col-7">
+                    <div class="card-2 p-3">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="inner-card animate-pulse">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-10">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-6">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-4">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="found_dog_skeleton d-flex justify-content-center align-items-center">
+        <div class="card">
+            <div class="row">
+                <div class="col-5">
+                    <div class="card-1 animate-pulse">
+                    </div>
+                </div>
+                <div class="col-7">
+                    <div class="card-2 p-3">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="inner-card animate-pulse">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-10">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-6">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-4">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="found_dog_skeleton d-flex justify-content-center align-items-center">
+        <div class="card">
+            <div class="row">
+                <div class="col-5">
+                    <div class="card-1 animate-pulse">
+                    </div>
+                </div>
+                <div class="col-7">
+                    <div class="card-2 p-3">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="inner-card animate-pulse">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-10">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-6">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-4">
+                                <div class="inner-card animate-pulse">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
