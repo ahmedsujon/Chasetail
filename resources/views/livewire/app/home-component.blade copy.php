cdaaa256979cde6c73e268472c8d2be71fe1cd44
ahmedsujon <sujonahmed424@gmail.com>
2024-03-20 11:37:39 +0600

    home page setup

---
+++ resources/views/livewire/app/home-component.blade copy.php
@@ -0,0 +1,39 @@
+<div>
+    <section id="banner">
+        <div class="banner">
+            <div class="banner-text">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-9 col-lg-8 col-9">
+                            <h3>Do You Want To find
+                                your lost dog? </h3>
+                            <a href="#">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Page content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-lg-6 col-sm-12">
+                <div class="content-box">
+                    <h2>List of Found Dogs</h2>
+                    <livewire:app.components.found-dog-component lazy />
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6 col-sm-12">
+                <div class="content-box">
+                    <h2>List of Lost Dogs</h2>
+                    <livewire:app.components.lost-dog-component lazy />
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <livewire:app.user.auth.login-component />
+    <livewire:app.user.auth.register-component />
+
+</div>
--- resources/views/livewire/app/home-component.blade.php
+++ resources/views/livewire/app/home-component.blade.php
@@ -1,12 +1,15 @@
 <div>
-    <section id="banner">
+    <!-- Banner -->
+    <section id="banner-section">
         <div class="banner">
             <div class="banner-text">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-9 col-lg-8 col-9">
+                        <div class="col-md-5 col-lg-5 col-6">
                             <h3>Do You Want To find
                                 your lost dog? </h3>
+                            <p>Find your new best friend. We’re here to help your lost pet find their way home. Browse
+                                pets from our network of your nearby shelters and rescues.</p>
                             <a href="#">Get Started</a>
                         </div>
                     </div>
@@ -14,26 +17,90 @@
             </div>
         </div>
     </section>
-
     <!-- Page content -->
     <div class="container">
         <div class="row">
-            <div class="col-md-6 col-lg-6 col-sm-12">
-                <div class="content-box">
-                    <h2>List of Found Dogs</h2>
-                    <livewire:app.components.found-dog-component lazy />
+            <div class="col-lg-12 col-md-12 col-12">
+                <div class="welcome">
+                    <h4>Our solutions</h4>
+                    <h3>Unleashing the power of Love, together</h3>
+                    <p>All dogs deserve Love. We help them find it. We harness the power of love to make communities and
+                        dog
+                        families closer, stronger, and healthier.</p>
                 </div>
             </div>
-            <div class="col-md-6 col-lg-6 col-sm-12">
-                <div class="content-box">
-                    <h2>List of Lost Dogs</h2>
-                    <livewire:app.components.lost-dog-component lazy />
+        </div>
+        <div class="content-top">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="article-box">
+                        <img class="img-fluid" src="{{ asset('assets/app/images/content-dog01.jpg') }}"
+                            alt="Content Dog">
+                        <h4><a href="#">Dogs</a></h4>
+                        <p>We’re here to help you find your dog. Free and easy way to search lost dogs to help them
+                            return
+                            home.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="article-box">
+                        <img class="img-fluid" src="{{ asset('assets/app/images/content-dog02.jpg') }}"
+                            alt="Content Dog">
+                        <h4><a href="#">Found Dogs</a></h4>
+                        <p>Help reunite found dogs, by snapping a photo and uploading it or register your dog in case
+                            they
+                            ever go missing. It’s free!</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="article-box">
+                        <img class="img-fluid" src="{{ asset('assets/app/images/content-dog03.jpg') }}"
+                            alt="Content Dog">
+                        <h4><a href="#">Donation</a></h4>
+                        <p>A little Love goes a long way. We can’t do this without you. Every dollar counts, just like
+                            every
+                            dog helps make the world a better place.</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <livewire:app.user.auth.login-component />
-    <livewire:app.user.auth.register-component />
-
+    <div class="challenge">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4>The challenge</h4>
+                    <h3>All dogs deserve Love. We help them find it</h3>
+                    <div class="challenge-content">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-4 col-12">
+                                <div class="challenge-box">
+                                    <img class="img-fluid" src="{{ asset('assets/app/images/icon-loving-home.png') }}"
+                                        alt="Millions of dogs">
+                                    <h5>Millions of dogs</h5>
+                                    <p>never find a loving home.</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-12">
+                                <div class="challenge-box">
+                                    <img class="img-fluid" src="{{ asset('assets/app/images/icon-missing-dog.png') }}"
+                                        alt="In every 1 in 10">
+                                    <h5>In every 1 in 10</h5>
+                                    <p>Dogs go missing and many never make it back home.</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-12">
+                                <div class="challenge-box">
+                                    <img class="img-fluid" src="{{ asset('assets/app/images/icon-resource.png') }}"
+                                        alt="Loving Home">
+                                    <h5>Countless</h5>
+                                    <p>shelters and organizations are under resourced.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
