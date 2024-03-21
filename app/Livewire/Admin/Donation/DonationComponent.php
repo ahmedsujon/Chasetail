c4c35015a938c6ae027fce0b3d0ad5f3a6486f12
ahmedsujon <sujonahmed424@gmail.com>
2024-03-21 12:22:14 +0600

    donation livewire

---
+++ app/Livewire/Admin/Donation/DonationComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Donation;
+
+use Livewire\Component;
+
+class DonationComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.donation.donation-component');
+    }
+}
