d9e092c299658e1cebc404027906dd9a2c9ee581
ahmedsujon <sujonahmed424@gmail.com>
2024-04-16 12:04:23 +0600

    assets update

--- resources/views/livewire/app/layouts/base.blade.php
+++ resources/views/livewire/app/layouts/base.blade.php
@@ -41,7 +41,6 @@
         });
     </script>
 
-    <script src="{{ asset('assets/app/js/jquery-3.7.1.min.js') }}"></script>
     <script src="{{ asset('assets/app/js/popper.min.js') }}"></script>
     <script src="{{ asset('assets/app/js/bootstrap.min.js') }}"></script>
     @stack('scripts')
