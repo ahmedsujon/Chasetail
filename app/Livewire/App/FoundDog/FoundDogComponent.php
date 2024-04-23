844453974f1d59917d1608d1ec5d34e92bf771e8
ahmedsujon <sujonahmed424@gmail.com>
2024-04-23 11:45:43 +0600

    dog breed list

--- app/Livewire/App/FoundDog/FoundDogComponent.php
+++ app/Livewire/App/FoundDog/FoundDogComponent.php
@@ -9,11 +9,24 @@ use Livewire\WithPagination;
 class FoundDogComponent extends Component
 {
     use WithPagination;
-    public $sortingValue = 12, $searchTerm;
+    public $sortingValue = 12, $searchTerm, $searchByGenderTerm, $searchByColorTerm;
     public function render()
     {
-        $found_dogs = FoundDog::where('name', 'like', '%' . $this->searchTerm . '%')->inRandomOrder()->paginate($this->sortingValue);
+
+
+        $found_dogs = FoundDog::where('name', 'like', '%' . $this->searchTerm . '%')
+
+        ->when($this->searchByGenderTerm !== null && $this->searchByGenderTerm !== '', function ($query) {
+            return $query->where('gender', $this->searchByGenderTerm);
+        })
+
+        ->when($this->searchByColorTerm !== null && $this->searchByColorTerm !== '', function ($query) {
+            return $query->where('color', $this->searchByColorTerm);
+        })
+
+        ->inRandomOrder()->paginate($this->sortingValue);
         $this->dispatch('reload_scripts');
+
         return view('livewire.app.found-dog.found-dog-component', ['found_dogs'=>$found_dogs])->layout('livewire.app.layouts.base');
     }
 }
