0e83aa7bace352b99a1074e9a3e35f4a99d864a7
ahmedsujon <sujonahmed424@gmail.com>
2024-05-16 12:07:24 +0600

    found dog search

--- resources/views/livewire/app/found-dog/found-dog-component.blade.php
+++ resources/views/livewire/app/found-dog/found-dog-component.blade.php
@@ -50,10 +50,11 @@
                                         Filters</a></span></h4>
                             <h5>Location</h5>
                             <div class="location-left">
-                                <input type="text" wire:model.live="searchByAddressTerm" name="" id="">
+                                <input class="form-control form-location" id="location_address" name="location_address"
+                                    autocomplete="off" type="text" placeholder="Found near (address or zip)">
                             </div>
                             <div class="current-location">
-                                <p><a href="#">Use current location</a></p>
+                                <p><a onclick="getCurrentLocation();">Use current location</a></p>
                             </div>
                             <div class="search-radius">
                                 <label for="formControlRange" class="form-label">Search Radius</label>
@@ -487,24 +488,43 @@
                 <div class="col-lg-9 col-md-8 col-12">
                     <div class="lost-dog-list-right">
                         <div class="row">
-                            @foreach ($found_dogs as $found_dog)
-                                <div class="col-lg-4 col-md-6 col-12">
-                                    <div class="lost-dog-list-right-text">
-                                        <img class="img-fluid" src="{{ asset($found_dog->images) }}" alt="Lost Dog">
-                                        <h4><a
-                                                href="{{ route('app.found.dogs.details', ['id' => $found_dog->id]) }}">{{ $found_dog->name }}</a>
-                                        </h4>
-                                        <p>Dallas - Texas:
-                                        <ul>
-                                            <li>{{ $found_dog->gender }}</li>
-                                        </ul>
-                                        </p>
+                            @if ($found_dogs->count() > 0)
+                                @php
+                                    $sl =
+                                        $found_dogs->perPage() * $found_dogs->currentPage() -
+                                        ($found_dogs->perPage() - 1);
+                                @endphp
+                                @foreach ($found_dogs as $found_dog)
+                                    <div class="col-lg-4 col-md-6 col-12">
+                                        <div class="lost-dog-list-right-text">
+                                            <img class="img-fluid" src="{{ asset($found_dog->images) }}"
+                                                alt="Lost Dog">
+                                            <h4><a
+                                                    href="{{ route('app.found.dogs.details', ['id' => $found_dog->id]) }}">{{ $found_dog->name }}</a>
+                                            </h4>
+                                            <p>Dallas - Texas:
+                                            <ul>
+                                                <li>{{ $found_dog->gender }}</li>
+                                            </ul>
+                                            </p>
+                                        </div>
                                     </div>
+                                @endforeach
+                            @else
+                                <tr>
+                                    <td style="padding-top: 100px;" colspan="7" class="text-center pt-5 pb-5">No
+                                        dogs found!</td>
+                                </tr>
+                            @endif
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-12">
+                                <div class="dog-pagination">
+                                    {{ $found_dogs->links('livewire.pagination-links') }}
                                 </div>
-                            @endforeach
+                            </div>
                         </div>
                     </div>
-                    {{ $found_dogs->links('livewire.pagination-links') }}
                 </div>
             </div>
         </div>
@@ -513,6 +533,27 @@
 
 @push('scripts')
     <script>
+        function getCurrentLocation() {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const p = position.coords;
+
+                $('#latitude').val(p.latitude);
+                $('#longitude').val(p.longitude);
+
+                @this.set('latitude', p.latitude);
+                @this.set('longitude', p.longitude);
+
+                reverseGeocode(p.latitude, p.longitude);
+                setLocation(p.latitude, p.longitude, '');
+
+            }, function(error) {
+                if (error.code === 1) {
+                    alert('Permission denied. Please allow location access in your browser settings.');
+                } else {
+                    alert('Error getting location: ' + error.message);
+                }
+            });
+        }
 
         function reverseGeocode(latitude, longitude) {
             const apiUrl =
@@ -525,15 +566,15 @@
                     if (data.display_name) {
                         // console.log(data.address);
 
-                        var road = data.address.road != undefined ? data.address.road + ', ' : '';
-                        var county = data.address.county != undefined ? data.address.county + ', ' : '';
-                        var postcode = data.address.postcode != undefined ? data.address.postcode + ', ' : '';
-                        var state = data.address.state != undefined ? data.address.state : '';
+                        // var road = data.address.road != undefined ? data.address.road + ', ' : '';
+                        // var county = data.address.county != undefined ? data.address.county + ', ' : '';
+                        // var postcode = data.address.postcode != undefined ? data.address.postcode + ', ' : '';
+                        // var state = data.address.state != undefined ? data.address.state : '';
 
-                        // const fullAddress = data.display_name;
-                        const fullAddress = road + county + postcode + state;
-                        // $('#location_address').val(fullAddress);
-                        $('#location').html(fullAddress);
+                        const fullAddress = data.display_name;
+                        // const fullAddress = road + county + postcode + state;
+                        $('#location_address').val(fullAddress);
+                        // $('#location_address').html(fullAddress);
                         @this.set('address', fullAddress);
                     }
                 })
@@ -542,21 +583,103 @@
                 });
         }
 
-        $(document).ready(function() {
-            $(".maps").each(function(index) {
-                var latitude = $(this).data('latitude');
-                var longitude = $(this).data('longitude');
-                var name = $(this).data('name');
-                let mapOptions = {
-                    center: [latitude, longitude],
-                    zoom: 10
-                }
-                let map = new L.map('map', mapOptions);
-                let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-                map.addLayer(layer);
-                let marker = new L.Marker([latitude, longitude]);
-                marker.addTo(map);
-            });
+        const resultsWrapperHTML = document.getElementById("search-result");
+        let mapOptions = {
+            center: [23.9456166, 90.2526382],
+            zoom: 10
+        }
+
+        let map = new L.map('map', mapOptions);
+
+        let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+        map.addLayer(layer);
+
+
+        let marker = null;
+        map.on('click', (event) => {
+
+            if (marker !== null) {
+                map.removeLayer(marker);
+            }
+
+            marker = L.marker([event.latlng.lat, event.latlng.lng]).addTo(map);
+
+            document.getElementById('latitude').value = event.latlng.lat;
+            document.getElementById('longitude').value = event.latlng.lng;
+
+            @this.set('latitude', event.latlng.lat);
+            @this.set('longitude', event.latlng.lng);
+            reverseGeocode(event.latlng.lat, event.latlng.lng);
         });
+
+
+        // search location handler
+        let typingInterval
+
+        // typing handler
+        function onTyping(e) {
+            clearInterval(typingInterval)
+            const {
+                value
+            } = e
+
+            typingInterval = setInterval(() => {
+                clearInterval(typingInterval)
+                searchLocation(value)
+            }, 100)
+        }
+
+        // search handler
+        function searchLocation(keyword) {
+            if (keyword) {
+                fetch(`https://nominatim.openstreetmap.org/search?q=${keyword}&format=json`)
+                    .then((response) => {
+                        return response.json()
+                    }).then(json => {
+                        if (json.length > 0) return renderResults(json)
+                    })
+            }
+        }
+
+        // render results
+        function renderResults(result) {
+            let resultsHTML = ""
+
+            result.map((n) => {
+                resultsHTML +=
+                    `<li><a href="javascript:void(0)" onclick="setLocation(${n.lat},${n.lon},'${n.display_name}')">${n.display_name}</a></li>`
+            })
+
+            resultsWrapperHTML.innerHTML = resultsHTML
+        }
+
+        // clear results
+        function clearResults() {
+            resultsWrapperHTML.innerHTML = ""
+        }
+
+        // set location from search result
+        function setLocation(lat, lon, display_name) {
+            // set map focus
+            map.setView(new L.LatLng(lat, lon), 10)
+
+            // regenerate marker position
+            if (marker !== null) {
+                map.removeLayer(marker);
+            }
+            marker = L.marker([lat, lon]).addTo(map);
+
+            document.getElementById('latitude').value = lat;
+            document.getElementById('longitude').value = lon;
+
+            $('#location_address').val(display_name);
+
+            @this.set('latitude', lat);
+            @this.set('longitude', lon);
+            @this.set('searchByAddressTerm', display_name);
+
+            // clear results
+            clearResults()
+        }
     </script>
 @endpush
